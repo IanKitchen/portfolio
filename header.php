ebcfea21383d0c6d893a289ed7742d3915ab7310
Ian Kitchen <ian@oceanrain.com>
2016-08-17 20:59:03 +0100

    root level

---
+++ header.php
@@ -0,0 +1,11 @@
+<div class="header">
+	<a class="link" href="index.html">
+		Overview 
+	</a>
+	<a class="link" href="principles.html">
+		Design Principles 
+	</a>
+	<a class="link" href="mda.html">
+		MDA 
+	</a>
+</div>
---
+++ test.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html >
+<html>
+	<head>
+		<link rel="stylesheet/less" type="text/css" href="css/styles.less"/>
+		<link rel="stylesheet" type="text/css" href="css/styles.css"/>
+		
+		<script type="text/javascript" src="js/jquery.js"></script>	
+		<script type="text/javascript" src="js/less.js"></script>
+		<script type="text/javascript" src="js/script.js" ></script>
+	<title>
+		Portfolio
+	</title>
+	</head>
+	<body>	
+	
+		<?php  include(__DIR__."/header.php"); ?>
+
+		<div class="content">
+		  	<img src="images\mda.jpg">
+		</div>
+	</body>
+</html>
