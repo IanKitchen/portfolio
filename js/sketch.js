8ca392547f00cf170829711ecbb789ffbc43c4b9
Ian Kitchen <ian@oceanrain.com>
2017-11-26 19:49:51 +0000

    Added stickies to home page

--- js/sketch.js
+++ js/sketch.js
@@ -1,13 +1,19 @@
 function setup() {
-	var myCanvas = createCanvas(800, 40);
-	myCanvas.parent('thumbnailsp5');
+	var myCanvas = createCanvas(700, 700);
+	myCanvas.parent('stickies');
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 16, 16);
+  //if (mouseIsPressed) {
+  //  fill(128);
+  //} else {
+  //  fill(255);
+  //}
+  
+  var stick = random(255);
+  
+  fill(random(255), random(255), random(255));
+  
+  if (stick <= 25)
+  	rect(mouseX-24, mouseY-24, 48, 48);
 }
